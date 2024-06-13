99c1e33df5d6f7aebaeb726c7ffcc3b55df589dc
Lucas Molon <lucasmolon@outlook.com>
2024-06-13 20:16:10 -0300

    Criação dos Models

---
+++ app/src/main/java/com/example/listadecomprassqlite/Models/ListaModel.java
@@ -0,0 +1,40 @@
+package com.example.listadecomprassqlite.Models;
+
+import java.util.List;
+
+public class ListaModel {
+    public ListaModel(Integer id, String descricao, String dataCriacao){
+        setId(id);
+        setDescricao(descricao);
+        setDataCriacao(dataCriacao);
+    }
+
+    private Integer id;
+    private String descricao;
+    private String dataCriacao;
+    private List<ProdutoModel> produtos;
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id = id;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDataCriacao(){
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(String dataCriacao){
+        this.dataCriacao = dataCriacao;
+    }
+}
